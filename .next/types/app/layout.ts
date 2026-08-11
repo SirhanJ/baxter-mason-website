81a81258ae35a15903e3a7ef1d4390bc889a8051
Sirhan <lixjwd@gmail.com>
2026-08-11 16:46:56 +0500

    sa

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: c:\Users\lixjw\Desktop\baxton'\baxter-mason-website\app\layout.tsx
+// File: C:\Users\lixjw\Desktop\baxton'\baxter-mason-website\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
